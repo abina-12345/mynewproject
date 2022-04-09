4eff0a9834fac249702e760af343adcd2a1d4cd1
abinash <dj.avinash55@gmail.com>
2022-04-09 09:50:23 +0530

    new2 created

---
+++ src/test/java/tests/New2.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class New2 {
+
+	public static void main(String[] args) {
+		
+		int a=1200;
+		
+		int b=2400;
+		
+		int c=b-a;
+		
+		System.out.println(c);
+		
+		
+
+	}
+
+}
