1eff82961d6b46c6bcf69e85e38b80144adfa8d5
abinash <dj.avinash55@gmail.com>
2022-04-09 09:39:19 +0530

    msg

---
+++ src/test/java/tests/New.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class New {
+
+	public static void main(String[] args) {
+		
+		int a=1200;
+		
+		int b=2400;
+		
+		int c=a+b;
+		
+		System.out.println(c);
+		
+		
+
+	}
+
+}
